87c64989f23f1cb1849a1f2542b4b70e4f4a4ee8
Nyyu <89911360+Nyyu@users.noreply.github.com>
2023-01-11 16:53:18 -0300

    chore (up-fi): start challenge

---
+++ challenges/ignite-template-reactjs-upload-de-imagens/src/pages/index.tsx
@@ -0,0 +1,81 @@
+import { Button as Btn, Box } from '@chakra-ui/react';
+import { useMemo } from 'react';
+import { useInfiniteQuery } from 'react-query';
+
+import { Header } from '../components/Header';
+import { CardList } from '../components/CardList';
+import { api } from '../services/api';
+import { Loading } from '../components/Loading';
+import { Error } from '../components/Error';
+
+type Image = {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+};
+
+type GetImageResponse = {
+  after: string;
+  data: Image[];
+};
+
+const handleFetchImages = async ({
+  pageParam = null,
+}): Promise<GetImageResponse> => {
+  const response = await api.get(`/api/images`, {
+    params: {
+      after: pageParam,
+    },
+  });
+  console.log('api response', response.data);
+  return response.data;
+};
+
+export default function Home(): JSX.Element {
+  const {
+    data,
+    isLoading,
+    isError,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery('images', handleFetchImages, {
+    getNextPageParam: lastPage => lastPage?.after || null,
+  });
+
+  const formattedData = useMemo(() => {
+    return data?.pages?.map(arr => arr?.data)?.flat();
+  }, [data]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    return <Error />;
+  }
+
+  return (
+    <>
+      <Header />
+
+      <Box maxW={1120} px={20} mx="auto" my={20}>
+        <CardList cards={formattedData} />
+        {hasNextPage && (
+          <>
+            {/* If I use chakra's button it throws an error ;-; */}
+            <button
+              type="button"
+              disabled={!hasNextPage || isFetchingNextPage}
+              onClick={() => fetchNextPage()}
+            >
+              {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+            </button>
+          </>
+        )}
+      </Box>
+    </>
+  );
+}
